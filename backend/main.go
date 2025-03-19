0279e2cd4ec6a2c23f6726ef5ba2299e37673723
Jim124 <fengpingan065@gmail.com>
2025-03-19 13:52:40 +0000

    add cors middleware

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	"github.com/go-compose-rest/database"
 	"github.com/go-compose-rest/routes"
 	"github.com/joho/godotenv"
@@ -22,6 +24,18 @@ func main() {
 	database.Init(mysqlHost, mysqlDb, mysqlUser, mysqlPassword)
 	port := os.Getenv("PORT")
 	server := gin.Default()
+	// server.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"https://localhost:5173"},
+	// 	AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
+	// 	AllowHeaders:     []string{"Origin"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	AllowOriginFunc: func(origin string) bool {
+	// 		return origin == "https://github.com"
+	// 	},
+	// 	MaxAge: 12 * time.Hour,
+	// }))
+	server.Use(cors.Default())
 	routes.RegisterServer(server)
 	server.Run(fmt.Sprintf(":%v", port))
 
